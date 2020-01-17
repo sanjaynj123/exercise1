805555c56c47bfb284abcf2b788decbbb887b82f
sanjaynj123 <sanjay123.com>
2020-01-17 17:53:22 +0530

    add a file

---
+++  17-01-20/calc.js
@@ -0,0 +1,32 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>calculator</title>
+ </head>
+ <body>
+ 	<textarea>text area</textarea>
+ 	<br><br>
+ 	<div col>
+ 	<button>9</button>
+ 	<button>8</button>
+ 	<button>7</button> 
+ 	<div col> 
+ 	<button>6</button>
+ 	<button>5</button>
+ 	<button>4</button>  
+ 	<div col>
+ 	<button>3</button>
+ 	<button>2</button>
+ 	<button>1</button>
+ 	<div col> 
+ 	<button>.</button>
+ 	<button>0</button>
+ 	<button>=</button>
+ 	<div col> 
+ 	<button>+</button>
+ 	<button>-</button>
+ 	<button>*</button>
+ 	<button>%</button>
+ </body>
+ <script type="text/javascript" src="calc.js"></script>
+ </html>
