204821acd3109d66f5bcf193b137fa231ab13e0b
sanjaynj123 <sanjay123.com>
2020-01-20 19:16:43 +0530

    create the file

---
+++  20-01-20/pallindrome/pallindrome.js
@@ -0,0 +1,13 @@
+function pallin()
+{
+	var i;
+ var str1 =";
+ var str2 =str.length;
+for (int i = str1.length; i<string; i--) {
+   String=str1.length[i];
+	}
+	
+	alert("is a pallindrome");
+  
+}
+str2=str1
